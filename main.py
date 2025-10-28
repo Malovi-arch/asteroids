76b9817d59711903e818170d2aec69b9d05808a3
Malovi-arch <it.sms@outlook.com>
2025-10-28 16:35:15 -0400

    Player Triangle

--- main.py
+++ main.py
@@ -4,13 +4,24 @@
 import pygame
 
 from constants import *
+from player import *
 
 def main():
     pygame.init()
 
+    x = SCREEN_WIDTH / 2
+
+    y = SCREEN_HEIGHT / 2
+
+    player1 = Player(x, y, 2)
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     BLACK = (0, 0, 0)
+
+    fps_clock = pygame.time.Clock()
+
+    dt = 0
 
     while True:
 
@@ -20,7 +31,13 @@ def main():
 
         screen.fill(BLACK)
 
+        player1.draw(screen)
+
         pygame.display.flip()
+
+        fps_clock.tick(60)
+
+        dt = (fps_clock.get_time() / 1000)
 
 if __name__ == "__main__":
     main()
